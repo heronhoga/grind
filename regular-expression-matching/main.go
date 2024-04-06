59af48efe3f893c2d93742465594d5639de9ca3a
heronhoga <nozzeravixer@gmail.com>
2024-04-06 15:40:54 +0700

    regular-expression-matching

---
+++ regular-expression-matching/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(isMatch("aa", "a*"))
+}
+
+func isMatch(s string, p string) bool {
+    if p == "" {
+        return s == ""
+    }
+
+    firstMatch := s != "" && (p[0] == s[0] || p[0] == '.')
+
+    if len(p) >= 2 && p[1] == '*' {
+        return isMatch(s, p[2:]) || (firstMatch && isMatch(s[1:], p))
+    } else {
+        return firstMatch && isMatch(s[1:], p[1:])
+    }
+}

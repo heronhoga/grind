46e1ba7e70eaf26564dcd7e45ea4e46e0b554688
heronhoga <nozzeravixer@gmail.com>
2024-04-16 14:43:31 +0700

    longest-common-prefix

---
+++ longest-common-prefix/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(longestCommonPrefix([]string{"flower", "flow", "flight"}))
+
+}
+
+func longestCommonPrefix(strs []string) string {
+    if len(strs) == 0 {
+        return ""
+    }
+
+    shortest := strs[0]
+    for _, str := range strs {
+        if len(str) < len(shortest) {
+            shortest = str
+        }
+    }
+
+    prefix := shortest
+    prefixLength := len(prefix)
+
+    for _, str := range strs {
+        for prefix != str[0:prefixLength] {
+            prefix = prefix[0 : prefixLength-1]
+            prefixLength--
+
+            if prefixLength < 1 {
+                return ""
+            }
+        }
+    }
+
+    return prefix
+}
+

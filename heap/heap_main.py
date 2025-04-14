dae1fea5212ab50c10486824de895c1ca91aa6a5
heronhoga <nozzeravixer@gmail.com>
2025-04-14 09:08:39 +0700

    fix:fix min heap and max heap heapify function

--- heap/heap_main.py
+++ heap/heap_main.py
@@ -1,4 +1,5 @@
 from max_heap import MaxHeap
+from min_heap import MinHeap
 print("--Max Heap--")
 heap = MaxHeap()
 
@@ -16,7 +17,36 @@ print("Extract the max", heap.extract_max())
 
 #print heap after extraction
 print("After extraction, heap: ", heap)
+
+print("Extract the max", heap.extract_max())
+
+#print heap after extraction
+print("After extraction, heap: ", heap)
+
 print("--Max Heap--\n")
 
 print("--Min Heap--")
+
+minHeap = MinHeap()
+
+minHeap.insert(5)
+minHeap.insert(9)
+minHeap.insert(2)
+minHeap.insert(3)
+minHeap.insert(7)
+minHeap.insert(4)
+
+#print heap
+print(minHeap)
+
+print("Extract the min", minHeap.extract_min())
+
+#print heap after extraction
+print("After extraction, heap: ", minHeap)
+
+print("Extract the min", minHeap.extract_min())
+
+#print heap after extraction
+print("After extraction, heap: ", minHeap)
+
 print("--Min Heap--")
--- heap/min_heap.py
+++ heap/min_heap.py
@@ -10,3 +10,49 @@ class MinHeap:
     
     def _right(self, i):
         return (2 * i) + 2
+    
+    def insert(self, key):
+        self.heap.append(key)
+        
+        i = len(self.heap) - 1
+        while i != 0 and self.heap[self._parent(i)] > self.heap[i]:
+            self.heap[self._parent(i)], self.heap[i] = self.heap[i], self.heap[self._parent(i)]
+            i = self._parent(i) 
+    
+    
+    def _heapify(self, i):
+        smallest = i
+        
+        left = self._left(i)
+        right = self._right(i)
+        
+        #check left
+        if i < len(self.heap) and left < len(self.heap) and self.heap[left] < self.heap[smallest]:
+            smallest = left
+        if i < len(self.heap) and right < len(self.heap) and self.heap[right] < self.heap[smallest]:
+            smallest = right
+            
+        if smallest != i:
+            self.heap[i], self.heap[smallest] = self.heap[smallest], self.heap[i]
+            self._heapify(smallest)
+        
+    def extract_min(self):
+        if not self.heap:
+            return None
+        
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        
+        root = self.heap[0]
+        
+        self.heap[0] = self.heap.pop()
+        self._heapify(0)
+        return root
+        
+    def get_min(self):
+        return self.heap[0] if self.heap else None
+    
+    def __str__(self):
+        return str(self.heap)
+        
+        
